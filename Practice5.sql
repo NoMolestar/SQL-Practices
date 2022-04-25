a653a836485d92a473f6316bc4ceb3ee785ac662
Carlos Moisés Chávez Jiménez <carlosmoiseschavezjimenez@gmail.com>
2022-04-25 09:26:18 -0500

    Added practice 5 and 6.

---
+++ Practice5.sql
@@ -0,0 +1,10 @@
+-- Contrains
+
+-- Primary keys
+-- Must be unique and not null.
+
+-- Foreign keys
+-- Must exist in the main table.
+
+-- Check / Check In
+-- These are constraints values where you can validate if the data exists and is correct.
